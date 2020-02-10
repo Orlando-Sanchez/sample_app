2e503f37fcd74a587340f738b62f8f038658b34a
S662 <or.sanchez100@gmail.com>
2020-02-10 13:12:31 -0400

    Add user microposts

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -14,6 +14,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get contact_path
     assert_select "title", full_title("Contact")
 
+    get signup_path
+    assert_select "title", full_title("Sign up")
+
     @user = users(:archer)
     log_in_as(@user)
     
@@ -26,12 +29,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    assert_select "a[href=?]", signup_path
 
     get users_path
     assert_select "title", full_title("All users")
     
-    get signup_path
-    assert_select "title", full_title("Sign up")
   end
 end
